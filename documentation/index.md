8be541a3c44e7d9da7b59d98cd45d4cd4b8dfdcc
Eli Mor <eli_mor.geo@yahoo.com>
2013-02-24 10:57:08 -0700

    Initial Release

---
+++ documentation/index.md
@@ -0,0 +1,35 @@
+# chooseintentmodule Module
+
+## Description
+
+This module allows to build customer Intentchooser for android, based on the installed packages.
+The module allows to verify if a certain pacakge is installed and 
+
+## Accessing the chooseintentmodule Module
+
+To access this module from JavaScript, you would do the following:
+
+	var chooseintentmodule = require("com.abp.chooseintent");
+
+The chooseintentmodule variable is a reference to the Module object.	
+
+## Reference
+
+TODO: If your module has an API, you should document
+the reference here.
+
+### ___PROJECTNAMEASIDENTIFIER__.isPackageAvailable
+
+Returns if a package is availeble
+
+## Usage
+
+Refer to app.js in the example folder
+
+## Author
+
+Eli Mor
+
+## License
+
+TODO: Free
---
+++ example/app.js
@@ -0,0 +1,173 @@
+// This is a test harness for your module
+// You should do something interesting in this harness
+// to test out the module and to provide instructions
+// to users on how to use it by example.
+
+// open a single window
+var win = Ti.UI.createWindow({
+	backgroundColor : 'white'
+});
+var chooseintent = require('com.abp.chooseintent');
+Ti.API.info("module is => " + chooseintent);
+
+//create object instance, a parasitic subclass of Observable
+var self = Ti.UI.createView();
+
+var shareButton = Titanium.UI.createButton({
+	title : 'Is Intent Available',
+	top : '10dp',
+	width : '45%',
+	left : '2dp',
+	height : 'auto'
+});
+
+var share2Button = Titanium.UI.createButton({
+	title : 'Get Packages',
+	top : '10dp',
+	width : '45%',
+	right : '2dp',
+	height : 'auto'
+});
+
+var share3Button = Titanium.UI.createButton({
+	title : 'Custom Chooser',
+	top : '60dp',
+	width : '45%',
+	left : '2dp',
+	height : 'auto'
+});
+
+var share4Button = Titanium.UI.createButton({
+	title : 'Show Chooser',
+	top : '60dp',
+	width : '45%',
+	right : '2dp',
+	height : 'auto'
+});
+
+var share5Button = Titanium.UI.createButton({
+	title : 'Empty Chooser',
+	top : '110dp',
+	width : '45%',
+	left : '2dp',
+	height : 'auto'
+});
+
+var share6Button = Titanium.UI.createButton({
+	title : 'Delete Intent',
+	top : '110dp',
+	width : '45%',
+	right : '2dp',
+	height : 'auto'
+});
+
+var intentField = Titanium.UI.createTextField({
+	value : 'android.intent.action.SEND',
+	top : '160dp',
+	left : 10,
+	height : 'auto',
+	width : '90%',
+	color : '#555',
+	textAlignment : 'left'
+});
+
+var packageField = Titanium.UI.createTextField({
+	value : 'twitter',
+	top : '210dp',
+	left : 10,
+	height : 'auto',
+	width : '90%',
+	color : '#555',
+	textAlignment : 'left'
+});
+
+var progressLabel = Titanium.UI.createLabel({
+	text : 'false',
+	top : '260dp',
+	left : 10,
+	height : 'auto',
+	width : '80%',
+	color : '#555',
+	textAlignment : 'center'
+});
+
+self.add(shareButton);
+self.add(share2Button);
+self.add(share3Button);
+self.add(share4Button);
+self.add(share5Button);
+self.add(share6Button);
+self.add(progressLabel);
+self.add(intentField);
+self.add(packageField);
+
+shareButton.addEventListener('click', function() {
+	var intent = {
+		action : intentField.value,
+		type : "text/plain",
+		packageStr : packageField.value
+	};
+	Ti.API.info('isPackageAvailable: ' + chooseintent.isPackageAvailable(intent));
+	progressLabel.text = chooseintent.isPackageAvailable(intent);
+});
+
+share2Button.addEventListener('click', function() {
+	var intent = {
+		action : intentField.value,
+		type : "text/plain",
+		packageStr : packageField.value
+	};
+	Ti.API.info('getPackages: ' + JSON.stringify(chooseintent.getPackages(intent)));
+	progressLabel.text = chooseintent.getPackages(intent)[0];
+});
+
+share3Button.addEventListener('click', function() {
+	var twit = {
+		action : Ti.Android.ACTION_SEND,
+		type : "text/plain",
+		packageStr : packageField.value
+	}
+	var twitterPack = chooseintent.getPackages(twit);
+	Ti.API.info(twitterPack);
+	var twitter = {
+		action : Ti.Android.ACTION_SEND,
+		type : "text/plain",
+		packageName : twitterPack[0]
+	}
+	chooseintent.addIntentToChooser(twitter);
+	chooseintent.putExtra(Ti.Android.EXTRA_SUBJECT, "subject to be twitted");
+	chooseintent.putExtra(Ti.Android.EXTRA_TEXT, "text to be twitted");
+	var facebook = {
+		action : Ti.Android.ACTION_SEND,
+		type : "text/plain",
+		packageName : 'com.facebook.katana'
+	}
+	chooseintent.addIntentToChooser(facebook);
+	var email = {
+		action : Ti.Android.ACTION_SEND,
+		type : "text/plain",
+		packageName : 'com.google.android.gm'
+	}
+	chooseintent.addIntentToChooser(email);
+	chooseintent.putExtra(Ti.Android.EXTRA_SUBJECT, "subject to be shared");
+	chooseintent.putExtra(Ti.Android.EXTRA_TEXT, "text to be shared");
+	chooseintent.showChooser('Share');
+});
+
+share4Button.addEventListener('click', function() {
+	chooseintent.showChooser('Share');
+});
+
+share5Button.addEventListener('click', function() {
+	chooseintent.emptyChooser();
+	chooseintent.showChooser('Share');
+});
+
+share6Button.addEventListener('click', function() {
+	chooseintent.removeIntentFromChooser('com.twitter.android');
+	chooseintent.showChooser('Share');
+});
+
+win.add(view);
+win.open();
+
